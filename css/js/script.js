21aadf58c4a3db15d780978eaf39aaed579b324a
Babi <barbarafonseca.dev@gmail.com>
2025-12-12 18:31:55 -0300

    pequenos ajustes

--- css/js/script.js
+++ css/js/script.js
@@ -1,14 +1,10 @@
-document.getElementById("botaoEnviar").addEventListener("click", validaFormulario )
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
 
-function validaFormulario(event){
-    event.preventDefault();
-  
-   if (document.getElementById("nome").value != "" && 
-       document.getElementById("email").value != "" &&
-       document.getElementById("telefone").value != ""){
-
-    alert("Prontinho! Você receberá as novidades por e-mail!")
+function validaFormulario(){
+  if(document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("numero").value != ""){
+    alert("Prontinho, você receberá as novidades por email.")
   }else{
-    alert("Preencha os campos nome e email!")
- } 
+    alert("Por favor, preencha os campos nome, email e telefone!")
+  }
 }
+  
